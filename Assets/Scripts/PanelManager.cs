daed3a7a159644b973302dddb95d426706c50c45
Oleg551 <brazhenko.oleg@yandex.ru>
2023-10-22 16:20:20 +0300

    new commit

--- Assets/Scripts/PanelManager.cs
+++ Assets/Scripts/PanelManager.cs
@@ -5,20 +5,17 @@ using UnityEngine.UI;
 
 public class PanelManager : MonoBehaviour
 {
-    public GameObject PanelEvents, PanelProfile, PanelReg, PanelBegin;
+    public GameObject PanelCl, PanelNotice, PanelLent, PanelMessage, PanelProfile, PanelReg, PanelBegin;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         PanelBegin.SetActive(true);
         PanelProfile.SetActive(false);
         PanelReg.SetActive(false); 
-        PanelEvents.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        PanelCl.SetActive(false);
+        PanelNotice.SetActive(false);
+        PanelLent.SetActive(false);
+        PanelMessage.SetActive(false);
     }
 
     public void BeginOnClick()
@@ -26,7 +23,10 @@ public class PanelManager : MonoBehaviour
         PanelBegin.SetActive(true);
         PanelProfile.SetActive(false);
         PanelReg.SetActive(false);
-        PanelEvents.SetActive(false);
+        PanelCl.SetActive(false);
+        PanelNotice.SetActive(false);
+        PanelLent.SetActive(false);
+        PanelMessage.SetActive(false);
     }
 
     public void RegOnClick()
@@ -34,15 +34,10 @@ public class PanelManager : MonoBehaviour
         PanelBegin.SetActive(false);
         PanelProfile.SetActive(false);
         PanelReg.SetActive(true);
-        PanelEvents.SetActive(false);
-    }
-
-    public void EventOnClick()
-    {
-        PanelBegin.SetActive(false);
-        PanelProfile.SetActive(false);
-        PanelReg.SetActive(false);
-        PanelEvents.SetActive(true);
+        PanelCl.SetActive(false);
+        PanelNotice.SetActive(false);
+        PanelLent.SetActive(false);
+        PanelMessage.SetActive(false);
     }
 
     public void ProfileOnClick()
@@ -50,6 +45,53 @@ public class PanelManager : MonoBehaviour
         PanelBegin.SetActive(false);
         PanelProfile.SetActive(true);
         PanelReg.SetActive(false);
-        PanelEvents.SetActive(false);
+        PanelCl.SetActive(false);
+        PanelNotice.SetActive(false);
+        PanelLent.SetActive(false);
+        PanelMessage.SetActive(false);
+    }
+
+    public void ClOnClick()
+    {
+        PanelBegin.SetActive(false);
+        PanelProfile.SetActive(false);
+        PanelReg.SetActive(false);
+        PanelCl.SetActive(true);
+        PanelNotice.SetActive(false);
+        PanelLent.SetActive(false);
+        PanelMessage.SetActive(false);
+    }
+
+    public void NoticeOnClick()
+    {
+        PanelBegin.SetActive(false);
+        PanelProfile.SetActive(false);
+        PanelReg.SetActive(false);
+        PanelCl.SetActive(false);
+        PanelNotice.SetActive(true);
+        PanelLent.SetActive(false);
+        PanelMessage.SetActive(false);
+    }
+
+    public void LentOnClick()
+    {
+        PanelBegin.SetActive(false);
+        PanelProfile.SetActive(false);
+        PanelReg.SetActive(false);
+        PanelCl.SetActive(false);
+        PanelNotice.SetActive(false);
+        PanelLent.SetActive(true);
+        PanelMessage.SetActive(false);
+    }
+
+    public void MessageOnClick()
+    {
+        PanelBegin.SetActive(false);
+        PanelProfile.SetActive(false);
+        PanelReg.SetActive(false);
+        PanelCl.SetActive(false);
+        PanelNotice.SetActive(false);
+        PanelLent.SetActive(false);
+        PanelMessage.SetActive(true);
     }
 }
